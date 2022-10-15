264a45710dfa61af8461c93ae01698a2e9e64af9
Ilyas Lkridah <ilkridah@e1r9p7.1337.ma>
2022-10-15 23:47:56 +0100

    learn_how_to_use_constructor

--- Module01/Zombie.hpp
+++ Module01/Zombie.hpp
@@ -1,9 +1,15 @@
 #ifndef ZOMBIE_HPP
 # define ZOMBIE_HPP
 
+#include"iostream"
+
 class Zombie
 {
     private:
-    
+        string::string name;
+    public:
+        Zombie();
+        ~Zombie();
+        void announce();
 }
 #endif
